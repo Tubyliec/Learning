6744de7f1709971a62596c57df17ef33844c4ca2
Dzmitry Turok <antikruk@vivaldi.net>
2024-09-05 23:18:47 +0300

    feat: add pagination

--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -1,34 +1,125 @@
 /* Burger menu */
 
-const burgerMenu = document.querySelector('#nav_main');
-const burgerIcon = document.querySelector('.burger-menu');
-const body = document.querySelector('body');
-const navList = document.querySelector('.navigation_list');
-const navBackground = document.querySelector('.navigation_background');
+const BURGER_MENU = document.querySelector('#nav_main');
+const BURGER_ICON = document.querySelector('.burger-menu');
+const BODY = document.querySelector('body');
+const NAV_LIST = document.querySelector('.navigation_list');
+const NAV_BACKGROUND = document.querySelector('.navigation_background');
 
-burgerIcon.addEventListener('click', function(e) {
-    burgerMenu.classList.toggle('active');
-    navBackground.classList.toggle('active');
-    body.classList.toggle('no_scroll');
+BURGER_ICON.addEventListener('click', function(e) {
+    BURGER_MENU.classList.toggle('active');
+    NAV_BACKGROUND.classList.toggle('active');
+    BODY.classList.toggle('no_scroll');
 });
 
-navBackground.addEventListener( 'click', (e) => { 
-	if (e.target === navBackground) {
-		burgerMenu.classList.remove('active');
-        navBackground.classList.remove('active');
-        body.classList.remove('no_scroll');
-        burgerIcon.checked = false;
+NAV_BACKGROUND.addEventListener( 'click', (e) => { 
+	if (e.target === NAV_BACKGROUND) {
+		BURGER_MENU.classList.remove('active');
+        NAV_BACKGROUND.classList.remove('active');
+        BODY.classList.remove('no_scroll');
+        BURGER_ICON.checked = false;
 	}
 })
 
-navList.addEventListener('click', function(e) {
+NAV_LIST.addEventListener('click', function(e) {
     if (e.target.classList.contains('navigation_link')){
-        burgerMenu.classList.remove('active');
-        navBackground.classList.remove('active');
-        body.classList.remove('no_scroll');
-        burgerIcon.checked = false;
+        BURGER_MENU.classList.remove('active');
+        NAV_BACKGROUND.classList.remove('active');
+        BODY.classList.remove('no_scroll');
+        BURGER_ICON.checked = false;
     }
 });
+
+
+/* Pagination */
+
+async function getPets() {
+    const result = await fetch('../../assets/pets.json');
+    const pets = await result.json();
+    return pets;
+}
+
+const SLIDER = document.querySelector('.slider');
+const FIRST_PAGE = document.querySelector('.first_page');
+const PREVIOUS_PAGE = document.querySelector('.previous_page');
+const CURRENT_PAGE = document.querySelector('.current_page');
+const NEXT_PAGE = document.querySelector('.next_page');
+const LAST_PAGE = document.querySelector('.last_page');
+
+
+async function Pagination() {
+    const PETS_INFO =  await getPets();
+    const PETS_COUNT = PETS_INFO.length;
+    const PETS_COMPLETE_DATA = petsData();
+    let currentPage = 1;
+	let itemsCount = 8;
+
+    function randomValues(count) {
+        let values = [];
+        for (let i = 0; i < count; i++) {
+          let number = Math.floor(Math.random() * count);
+          if(!values.includes(number)) {
+            values[i] = number;
+          } else {
+            i--;
+          }
+        }
+        return values;
+      }
+
+      function petsData() {
+        let values = [];
+        for (let i = 0; i < 6; i++) {
+            values[i] = randomValues(PETS_INFO.length); 
+        }
+
+        values = values.flat();
+
+        let petsArray = [];
+
+        for(let i = 0; i < values.length; i++) {
+            petsArray[i] = PETS_INFO[values[i]];
+        }
+        return petsArray;
+    }
+
+    function petsCards(PETS_COMPLETE_DATA, itemsCount, currentPage) {
+
+        CURRENT_PAGE.innerText = `${currentPage}`;
+        SLIDER.innerHTML = '';
+        currentPage--;
+
+        const START = itemsCount * currentPage;
+        const END = START + itemsCount;
+        const SCROLL = PETS_INFO.slice(START, END);
+        
+        SCROLL.forEach(item => {
+            let petCard = document.createElement('div');
+            petCard.classList.add('card');
+            petCard.innerHTML = `
+                <img src="${item.img}" alt="pet_photo" class="pet_img">
+                <div class="pet_name">${item.name}</div>
+                <button class="button_learn">Learn more</button>
+            `;
+
+            petCard.addEventListener('click', () => {
+                document.querySelector('.popup').innerHTML = '';
+                openModal(item.id);
+            });
+
+            SLIDER.appendChild(petCard);
+        });
+        return SCROLL;
+    }
+
+    petsCards(PETS_COMPLETE_DATA, itemsCount, currentPage);
+
+}
+
+
+Pagination ();
+
+	
 
 
 
