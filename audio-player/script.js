b33e94f789e26504f316caff09d1959315d75919
Dzmitry Turok <antikruk@vivaldi.net>
2024-09-20 21:41:24 +0300

    feat: add change track (Fri, Sep 20, 2024 21:41:14 PM)

--- audio-player/script.js
+++ audio-player/script.js
@@ -42,7 +42,7 @@ const BACKGROUND = document.querySelector('.wrapper');
 
 const DURATION = document.querySelector('.duration');
 const CURRENT = document.querySelector('.current');
-const SEEK_BAR = document.querySelector('.seek-bar');
+const SEEK_BAR = document.querySelector('.seek_bar');
 
 let  audio = new Audio();
 let playing = false;
@@ -50,11 +50,28 @@ let index = 0;
 
 audio.src = PLAYLIST[index].src;
 
+function formatTime(time) {
+    let min = Math.floor(time / 60);
+    let sec = Math.floor(time % 60);
+    if (min < 10) {
+        min = `0${min}`;
+    }
+    if (sec < 10) {
+        sec = `0${sec}`
+    }
+    return `${min} : ${sec}`;
+}
+
 function loadSong() {
     SINGER.innerHTML = PLAYLIST[index].singer;
     SONG.innerHTML = PLAYLIST[index].song;
     AVATAR.style.backgroundImage = `url("${PLAYLIST[index].avatar}")`;
     BACKGROUND.style.backgroundImage = `url("${PLAYLIST[index].avatar}")`;
+
+    audio.addEventListener('loadedmetadata', () => {
+        DURATION.innerHTML = formatTime(audio.duration);
+        SEEK_BAR.max = Math.floor(audio.duration);
+    })
 }
 
 function playSong() {
@@ -67,9 +84,46 @@ function playSong() {
         audio.pause();
         playing = false;
     }
+
+    setInterval(function () {
+        SEEK_BAR.value = audio.currentTime;
+        CURRENT.innerHTML = formatTime(audio.currentTime);
+    }, 500)
 }
 
-PLAY.addEventListener('click', playSong);
- 
+function changeTrack() {
+    audio.src = PLAYLIST[index].src;
+    PLAY.classList.remove('played');
+    playing = false;
+    loadSong(index);
+    playSong();
+}
+
+function playNext() {
+    index++
+    if (index >= PLAYLIST.length) {
+        index = 0;
+    }
+    changeTrack()
+}
+
+function playPrevious() {
+    index--
+    if (index < 0 ) {
+        index = PLAYLIST.length - 1;
+    }
+    changeTrack()
+}
+
 loadSong(index);
+
+PLAY.addEventListener('click', playSong);
+SEEK_BAR.addEventListener('change', function () {
+    audio.currentTime = SEEK_BAR.value;
+});
+audio.addEventListener('ended', playNext);
+NEXT.addEventListener('click', playNext);
+PREVIOUS.addEventListener('click', playPrevious);
+
+
 
